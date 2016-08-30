cf5126ca5865dd4bcb1798e39528f052a827c489
chiragrtr <chiragrtr@gmail.com>
2016-08-30 11:32:38 +0530

    Removed service package and added the service methods in the DealerController class

---
+++ src/main/java/com/cdk/dealersnetwork/controller/DealerController.java
@@ -0,0 +1,35 @@
+package com.cdk.dealersnetwork.controller;
+
+/**
+ * Created by sharmach on 30/8/2016.
+ */
+public class DealerController {
+
+    public static void createBroadcast(int dealerId, String make, String model, String color) {
+
+    }
+
+    public static void showMyBroadcasts(int dealerID) {
+
+    }
+
+    public static void showResponses(int dealerId, int broadcastId) {
+
+    }
+
+    public static void selectDeal(int dealerId, int broadcastId, int bidId) {
+
+    }
+
+    public static void rejectAllDeals(int dealerId, int broadcastId) {
+
+    }
+
+    public static void showOthersBroadcasts(int dealerId) {
+
+    }
+
+    public static void sendResponse(int dealerId, int broadcastId, int bidPrice, int deliveryHours) {
+
+    }
+}
