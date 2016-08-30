6c1c96ecc77f4086c7e0f8a1e7020a272a3a3282
chiragrtr <chiragrtr@gmail.com>
2016-08-30 16:13:26 +0530

    modified BidDAO

--- src/main/java/com/cdk/dealersnetwork/dao/BidDAO.java
+++ src/main/java/com/cdk/dealersnetwork/dao/BidDAO.java
@@ -1,8 +1,22 @@
 package com.cdk.dealersnetwork.dao;
 
+import org.springframework.orm.hibernate4.HibernateTemplate;
+
 /**
  * Created by malir on 8/30/2016.
  */
 public class BidDAO {
+    private HibernateTemplate hibernateTemplate;
 
+    public HibernateTemplate getHibernateTemplate() {
+        return hibernateTemplate;
+    }
+
+    public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
+    }
+
+    public void selectDeal(){
+
+    }
 }
