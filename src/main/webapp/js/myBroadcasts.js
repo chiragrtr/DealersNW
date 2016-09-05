493bf385399565ccdc49db2fe6e55be934a759be
shubhraj <shubhampatil132@gmail.com>
2016-09-05 12:08:06 +0530

    cancel button float right, myBroadcast look changed

--- src/main/webapp/js/myBroadcasts.js
+++ src/main/webapp/js/myBroadcasts.js
@@ -46,7 +46,7 @@ function createOpenBroadcastList(records) {
             "<ul class='myUl'>";
 
         var totalBids = records[i].totalBids;
-        htmlText += "<p>" + records[i].make + " " + records[i].model + " " + records[i].color + " " + records[i].broadcastDate + " " + totalBids + " " + "<button type='button' onclick=cancelBroadcast(" + records[i].broadcastId + ")>Cancel</button> ";
+        htmlText += "<p>" + records[i].make + " " + records[i].model + " " + records[i].color + " " + records[i].broadcastDate + " " + totalBids + " " + "<button id = 'cancelBtn' type='button' onclick=cancelBroadcast(" + records[i].broadcastId + ")>Cancel</button> ";
         if (totalBids > 0) {
             htmlText += "<div id='myUlDiv'><ul class='nav'>";
             for (j = 0; j < totalBids; j++) {
